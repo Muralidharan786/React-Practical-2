ff5e0aff936c0704e7c25924602d43f398456bdc
Muralidharan <114714388+Muralidharan786@users.noreply.github.com>
2026-02-06 17:38:57 +0530

    Uncomment MemoComputation component in App

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   return (
     <>
     {/* <MemoParent />  */}
-     {/* <MemoComputation />   */}
+    <MemoComputation /> 
      <CallbackExample />
     <ParentComponent /> 
 
